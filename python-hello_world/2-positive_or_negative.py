5d2a8a5035f6d05c6f92cee01967e3eb619d8184
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-01 10:19:30 +0100

    random numbers

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,18 +3,10 @@
 import random
 number = random.randint(-100, 100)
 print(number)
-number = number 
-
 if number > 0:
      print("number is positive")
 elif number == 0:
     print("number is zero")
 elif number < 0:
     print("number is negative")
-
-else:
-    print("negative function")
-
-
-
 
