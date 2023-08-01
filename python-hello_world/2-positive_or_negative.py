c35406cfb42a90e95a2c7b88436eea90545b6f73
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-01 09:40:38 +0100

    random numbers

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 
 import random
-
-def generate_random_signed_number():
-    return random.randint(-10, 10)
-
-def check_number_sign(number):
-    if number > 0:
-        return "is positive"
-    elif number == 0:
-        return "is zero"
-    else:
-        return "is negative"
-
-if __name__ == "__main__":
-    number = generate_random_signed_number()
-    print("The number:", number, "-", check_number_sign(number))
-
+number = random.randint(-10, 10)
+list (range(0, 10))
+if number > 0:
+     print("is positive")
+elif number == 1:
+    print("number is 0")
 
 
