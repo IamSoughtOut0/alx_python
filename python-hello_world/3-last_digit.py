52fbc5bc2163acc9a3ea3838825579936e9eda68
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-02 04:13:26 +0100

    random numbers

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
 import random
-number = random.randint(-10, 10)
-print(random.randint(-10,10))
+number = random.randint(-10000, 10000)
+print(number)
 if number > 0:
      print("number is positive")
 elif number == 0:
     print("number is zero")
-"""else number < 0:
+elif number < 0:
     print("number is negative")
-    """
-
